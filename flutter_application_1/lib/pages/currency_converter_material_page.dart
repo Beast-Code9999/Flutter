590c4e1533b39b3099e87d61560fbbc971ff4c9e
beastcode9999 <beastcode9999@gmail.com>
2026-02-12 11:32:14 +0930

    feat: Add textfield

--- flutter_application_1/lib/pages/currency_converter_material_page.dart
+++ flutter_application_1/lib/pages/currency_converter_material_page.dart
@@ -5,12 +5,43 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    // add border 
+    final border = OutlineInputBorder(
+                  borderSide: const BorderSide(
+                    color: Colors.black,
+                    width: 2.0,
+                  ),
+                  borderRadius: BorderRadius.circular(50));
+
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(160, 203, 255, 1),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
-          children: [Text("0")],
+          children: [
+            const Text(
+              "0",
+              style: TextStyle(
+                fontSize: 45,
+                fontWeight: FontWeight.w800,
+                color: Color.fromARGB(255, 0, 0, 0),
+              ),
+            ),
+            TextField(
+              style: TextStyle(color: const Color.fromARGB(255, 0, 0, 0)),
+              decoration: InputDecoration(
+                hintText: 'Please enter the amount in USD',
+                hintStyle: TextStyle(color: Color.fromARGB(179, 0, 0, 0)),
+                suffixIcon: Icon(Icons.monetization_on_outlined),
+                suffixIconColor: Color.fromARGB(255, 0, 0, 0),
+                filled: true,
+                fillColor: Colors.white,
+                focusedBorder: border,
+                enabledBorder: border,
+              ),
+            ),
+          ],
         ),
       ),
     );
