a275bb2c5fbb69953792377536214ffe3d93ae0c
beastcode9999 <beastcode9999@gmail.com>
2026-02-16 08:49:43 +0930

    feat: use StyleForm to avoid repeating widgetStatePropertyAll()

--- flutter_application_1/lib/pages/currency_converter_material_page.dart
+++ flutter_application_1/lib/pages/currency_converter_material_page.dart
@@ -56,12 +56,20 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
             Padding(
               padding: const EdgeInsets.all(10.0),
               child: ElevatedButton (
-                style: const ButtonStyle(
-                  elevation: WidgetStatePropertyAll(10),
-                  backgroundColor: WidgetStatePropertyAll(Colors.blue),
-                  foregroundColor: WidgetStatePropertyAll(Colors.white),
-                  minimumSize: WidgetStatePropertyAll(Size(double.infinity, 50)),
-                  shape: WidgetStatePropertyAll(RoundedRectangleBorder())
+                // style: const ButtonStyle(
+                //   elevation: WidgetStatePropertyAll(10),
+                //   backgroundColor: WidgetStatePropertyAll(Colors.blue),
+                //   foregroundColor: WidgetStatePropertyAll(Colors.white),
+                //   minimumSize: WidgetStatePropertyAll(Size(double.infinity, 50)),
+                //   shape: WidgetStatePropertyAll(RoundedRectangleBorder())
+                // ),
+
+                style: ElevatedButton.styleFrom(
+                //   elevation: WidgetStatePropertyAll(10),
+                  backgroundColor: (Colors.blue),
+                  foregroundColor: (Colors.white),
+                  minimumSize: (const Size(double.infinity, 50)),
+                  shape: (const RoundedRectangleBorder())
                 ),
                 onPressed: () {},
                 child: const Text('Convert'),
